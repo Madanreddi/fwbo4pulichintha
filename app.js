a8f09500906beffd09d4befdf52c68facc26ac3d
Madanreddi <89476569+Madanreddi@users.noreply.github.com>
2021-11-03 21:03:56 -0500

    added

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var motorcycleRouter = require('./routes/motorcycle');
-var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/motorcycle', motorcycleRouter);
-app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
